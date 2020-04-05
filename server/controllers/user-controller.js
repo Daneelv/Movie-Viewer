33f2d9403065ce1e3be77261e8e5c4ac5d095169
Daneel van Tonder <dnlvantonder@gmail.com>
2020-04-05 12:49:58 +0200

    Added Remove from favourites functionality

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,50 +1,44 @@
 const User = require('../models/user-model.js');
 
-
 // Create and Save a new user
 exports.create = (req, res) => {
-    // Validate request
-    if (!req.body.content) {
-        return res.status(400).send({
-            message: "User details can not be empty"
-        });
-    }
-
-    // Create a Note
-    const user = new User({
-        userName: req.body.userName || "Untitled User",
-        password: req.body.password,
-        loggedIn: req.body.loggedIn || false,
-        favourites: req.body.favourites || []
+  // Validate request
+  if (!req.body.content) {
+    return res.status(400).send({
+      message: 'User details can not be empty',
     });
+  }
 
-    // Save Note in the database
-    user.save()
-        .then(data => {
-            res.send(data);
-        }).catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while creating the new user."
-            });
-        });
+  // Create a User
+  const user = new User({
+    userName: req.body.userName || 'Untitled User',
+    password: req.body.password,
+    loggedIn: req.body.loggedIn || false,
+    favourites: req.body.favourites || [],
+  });
+
+  // Save User in the database
+  user
+    .save()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || 'Some error occurred while creating the new user.',
+      });
+    });
 };
 
 // Retrieve and return all users from the database.
-exports.findAll = (req, res) => {
-
-};
+exports.findAll = (req, res) => {};
 
 // Find a single user with a userId
-exports.findOne = (req, res) => {
-
-};
+exports.findOne = (req, res) => {};
 
 // Update a user identified by the userId in the request
-exports.update = (req, res) => {
-
-};
+exports.update = (req, res) => {};
 
 // Delete a user with the specified userId in the request
-exports.delete = (req, res) => {
-
-};
+exports.delete = (req, res) => {};
--- server/server.js
+++ server/server.js
@@ -5,10 +5,10 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Configuring the database
 const dbConfig = require('./config/database-config.js');
@@ -17,29 +17,30 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");
-}).catch(err => {
+mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Successfully connected to the database');
+  })
+  .catch((err) => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
-});
-
+  });
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({ "message": "Welcome to The movie DB Database" });
+  res.json({ message: 'Welcome to The movie DB Database' });
 });
-
 
 //User db route setup
 require('./routes/user-routes.js')(app);
 
 // listen for requests
 app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+  console.log('Server is listening on port 3000');
 });
-
 
 //https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/
